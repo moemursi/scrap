d8ff0b3dc49cfd9217c8f42ac565512ba895ff6f
moemursi <superman>
2018-08-26 01:37:57 -0400

    sec

--- nba_scraper.py
+++ nba_scraper.py
@@ -1,18 +1,40 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-driver = webdriver.PhantomJS(executable_path=r'/Users/me/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
+class Player():
+    def __init__(self):
+        self.name = ""
+        self.link = ""
 
-url = 'https://stats.nba.com/players/list'
-base_url ='https://stats.nba.com'
-driver.get(url)
 
-print(driver.page_source)
+def get_player():
 
-soup = BeautifulSoup(driver.page_source,'html.parser')
+    driver = webdriver.PhantomJS(executable_path=r'/Users/me/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
 
-div = soup.find('div' , class_ = 'stats-player-list')
-for a in div.find_all('a'):
-    print(a.text)
-    #print(base_url+a['href'])
-#print(div)
+    url = 'https://stats.nba.com/players/list'
+    base_url ='https://stats.nba.com'
+    driver.get(url)
+
+    print(driver.page_source)
+
+    soup = BeautifulSoup(driver.page_source,'html.parser')
+
+    div = soup.find('div' , class_ = 'stats-player-list')
+
+    players = []
+    for a in div.find_all('a'):
+        name = a.text
+        link = base_url+a['href']
+        new_player = Player()
+        new_player.name = name
+        new_player.link = link
+        players.append(new_player)
+    for player in players:
+        print("Name :: " + player.name )
+        print("Link :: " + player.link)
+    #print(div)
+    driver.quit()
+
+    return players
+
+get_player()
